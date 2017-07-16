c36ef2aff6d48da37e57ad6303464998e939cf56
saethlin <kimockb@gmail.com>
2017-07-16 15:39:10 -0400

    adding tests, fixing bug with derivative length as a result

--- src/profile.rs
+++ src/profile.rs
@@ -22,6 +22,7 @@ impl Profile {
         let der = ccf_diff
                 .zip(rv_diff)
                 .map(|(c, r)| c/r)
+                .chain(std::iter::once(0.0))
                 .collect::<Vec<f64>>();
 
         Profile {
@@ -56,8 +57,73 @@ impl Profile {
             self.ccf.iter().zip(self.derivative.iter())
             .skip((-quotient) as usize)
             .map(|(ccf, der)| ccf - remainder * der)
-            .chain(iter::repeat(self.ccf[0]).take((-quotient) as usize))
+            .chain(iter::repeat(self.ccf[0]).take((-quotient) as usize))  // TODO is this +1 correct?
             .collect::<Vec<f64>>()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use sun_ccfs::*;
+
+    #[test]
+    fn derivative_len_test() {
+        let test_profile = Profile::new(rv(), ccf_quiet());
+        assert_eq!(test_profile.derivative.len(), ccf_quiet().len(),
+        "derivative length is {} but should be {}", test_profile.derivative.len(), ccf_quiet().len());
+    }
+
+    #[test]
+    fn zero_shift_len_test() {
+        let test_profile = Profile::new(rv(), ccf_quiet());
+        let shifted = test_profile.shift(0.0);
+        assert_eq!(shifted.len(), ccf_quiet().len(),
+        "zero-shifted length is {} but should be {}", shifted.len(), ccf_quiet().len());
+    }
+
+    #[test]
+    fn neg_zero_shift_len_test() {
+        let test_profile = Profile::new(rv(), ccf_quiet());
+        let shifted = test_profile.shift(0.0);
+        assert_eq!(shifted.len(), ccf_quiet().len(),
+        "negative zero-shifted length is {} but should be {}", shifted.len(), ccf_quiet().len());
+    }
+
+    #[test]
+    fn shift_pos_len_test() {
+        let test_profile = Profile::new(rv(), ccf_quiet());
+        let shifted = test_profile.shift(1.5);
+        assert_eq!(shifted.len(), ccf_quiet().len(),
+        "postive-shifted length is {} but should be {}", shifted.len(), ccf_quiet().len());
+    }
+
+    #[test]
+    fn shift_neg_len_test() {
+        let test_profile = Profile::new(rv(), ccf_quiet());
+        let shifted = test_profile.shift(-1.5);
+        assert_eq!(shifted.len(), ccf_quiet().len(),
+        "negative-shifted length is {} but should be {}", shifted.len(), ccf_quiet().len());
+    }
+
+    #[test]
+    fn zero_shift_pos_test(){
+        let test_profile = Profile::new(rv(), ccf_quiet());
+        let shifted = test_profile.shift(0.0);
+        for (original, shifted) in ccf_quiet().iter().zip(shifted.iter()) {
+            assert_eq!(original, shifted,
+            "zero-shifted profile value should is {} but should be {}", original, shifted);
+        }
+    }
+
+    #[test]
+    fn zero_shift_neg_test(){
+        let test_profile = Profile::new(rv(), ccf_quiet());
+        let shifted = test_profile.shift(-0.0);
+        for (original, shifted) in ccf_quiet().iter().zip(shifted.iter()) {
+            assert_eq!(original, shifted,
+            "negative zero-shifted profile value should is {} but should be {}", original, shifted);
         }
     }
 }
