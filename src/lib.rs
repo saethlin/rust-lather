e82ab433b6a7aec7dccaf6ca2b54bade33496967
saethlin <kimockb@gmail.com>
2017-07-24 13:39:20 -0400

    adding bisector output at Python interface, fixed Cargo.toml, adding TODO list

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,9 @@ py_module_initializer!(rather, initrather, PyInit_rather, |py, m| {
 });
 
 py_class!(class Simulation |py| {
+
     data sim: RefCell<RustSimulation>;
+
     def __new__(_cls, filename: &str) -> PyResult<Simulation> {
         Simulation::create_instance(py, RefCell::new(RustSimulation::new(filename)))
     }
@@ -49,11 +51,22 @@ py_class!(class Simulation |py| {
         Ok(flux.into_pyarray(py, &np))
     }
 
-    def observe_rv(&self, time: PyArray, wavelength_min: f64, wavelength_max: f64) -> PyResult<PyArray> {
+    def observe_rv(&self, time: PyArray, wavelength_min: f64, wavelength_max: f64) -> PyResult<(PyArray, PyArray)> {
         let np = PyArrayModule::import(py)?;
         let time: ArrayViewD<f64> = time.as_array().into_pyresult(py, "time must be an array of f64")?;
         let time_vec: Vec<f64> = time.iter().cloned().collect();
-        let rv: Vec<f64> = self.sim(py).borrow_mut().observe_rv(&time_vec, wavelength_min, wavelength_max).iter().map(|o| o.rv).collect();
-        Ok(rv.into_pyarray(py, &np))
+        let observations = self.sim(py).borrow_mut().observe_rv(&time_vec, wavelength_min, wavelength_max);
+
+        let rv: Vec<f64> = observations.iter().map(|o| o.rv).collect();
+        let mut bis_data = Vec::<f64>::with_capacity(rv.len() * observations[0].bisector.len());
+        for bisector in observations.iter().map(|o| o.bisector.clone()) {
+            bis_data.extend(bisector);
+        }
+        let bisectors = PyArray::new::<f64>(py, &np, &[rv.len(), observations[0].bisector.len()]);
+        for (input, mut output) in bis_data.iter().zip(bisectors.as_slice_mut().unwrap()) {
+            *output = *input;
+        }
+
+        Ok((rv.into_pyarray(py, &np), bisectors))
     }
 });
