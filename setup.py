d1d44815f191e49519547dbf56783a75cd95ee12
saethlin <kimockb@gmail.com>
2017-08-25 01:48:20 -0400

    adding license file and setuptools options per pypi tutorial

--- setup.py
+++ setup.py
@@ -2,11 +2,23 @@ from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
 setup(name='lather',
-    version='0.1.0',
+    version='0.0.0',
+    description='A Python extension for modeling starspot effects on radial'
+    'velocity and photometric observations, inspired by complaints about the'
+    'SOAP project of similar goals.',
+    url='https://github.com/saethlin/rust-lather',
+    author='Benjamin Kimock',
+    author_email='kimockb@gmail.com',
+    license='MIT/Apache-2.0',
+    keywords='science simulation astronomy',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+    ]
     rust_extensions=[
         RustExtension('lather.lather', 'Cargo.toml', binding=Binding.RustCPython)
     ],
     packages=['lather'],
     zip_safe=False,
-    install_requires=['numpy']
+    install_requires=['numpy'],
+    python_requires='3.6',
 )
