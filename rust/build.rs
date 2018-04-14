e1bd2d5793be887468ffce1d23d57445dc0c1201
Ben Kimock <kimockb@gmail.com>
2018-04-14 07:12:45 -0400

    add the required milksnake FFI layer and Python class

--- rust/build.rs
+++ rust/build.rs
@@ -7,7 +7,6 @@ fn main() {
     let mut config: cbindgen::Config = Default::default();
     config.language = cbindgen::Language::C;
     cbindgen::generate_with_config(&crate_dir, config)
-      .unwrap()
-      .write_to_file("target/lather.h");
+        .unwrap()
+        .write_to_file("target/lather.h");
 }
-
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -39,9 +39,99 @@ mod spot;
 mod profile;
 mod sun_ccfs;
 
-/// Test function
+use std::ffi::{CStr, CString};
+use std::os::raw::c_char;
+
+/// Build a simulation from a path to a config file
 #[no_mangle]
-pub unsafe extern "C" fn a_function_from_rust() -> i32 {
-    42
+pub unsafe extern "C" fn simulation_new(filename: *const c_char) -> *mut Simulation {
+    let mut obj = Box::new(Simulation::new(CStr::from_ptr(filename).to_str().unwrap()));
+
+    let ptr: *mut _ = &mut *obj;
+
+    // Forget discards its argument (passed by-move), without triggering its
+    // destructor, if it has one.
+    ::std::mem::forget(obj);
+
+    ptr
 }
 
+/// Close down a simulation
+#[no_mangle]
+pub unsafe extern "C" fn simulation_free(sim: *mut Simulation) {
+    if sim.is_null() {
+        return;
+    }
+
+    // Now, we know the pointer is non-null, we can continue.
+    let obj: Box<Simulation> = ::std::mem::transmute(sim);
+
+    // Explicitly drop the object, unnecessary but nice
+    ::std::mem::drop(obj);
+}
+
+/// Print a simulation
+#[no_mangle]
+pub unsafe extern "C" fn simulation_tostring(sim: *mut Simulation) -> *const c_char {
+    use std::fmt::Write;
+    if sim.is_null() {
+        return 0 as *const c_char;
+    }
+
+    let mut output = String::new();
+    write!(&mut output, "{:#?}", *sim).unwrap();
+    let output = CString::new(output).unwrap();
+    let ptr = output.as_ptr();
+    std::mem::forget(output);
+    ptr
+}
+
+/// Observe the flux of a simulation at given time values in days
+#[no_mangle]
+pub unsafe extern "C" fn simulation_observe_flux(
+    sim: *mut Simulation,
+    times: *mut f64,
+    n_times: usize,
+    wave_start: f64,
+    wave_end: f64,
+) -> *const f64 {
+    if sim.is_null() {
+        return 0 as *const f64;
+    }
+    let time_slice = std::slice::from_raw_parts(times, n_times);
+    let output = (*sim).observe_flux(
+        time_slice,
+        wave_start,
+        wave_end,
+    );
+    let ptr = output.as_ptr();
+    std::mem::forget(output);
+    ptr
+}
+
+/// Observe the rv and bisectors of a simulation at given time values in days
+#[no_mangle]
+pub unsafe extern "C" fn simulation_observe_rv(
+    sim: *mut Simulation,
+    times: *mut f64,
+    n_times: usize,
+    wave_start: f64,
+    wave_end: f64,
+) -> *const f64 {
+    if sim.is_null() {
+        return 0 as *const f64;
+    }
+
+    let time_slice = std::slice::from_raw_parts(times, n_times);
+    let observations = (*sim).observe_rv(time_slice, wave_start, wave_end);
+    let mut output = Vec::with_capacity(n_times * 1001);
+    for ob in &observations {
+        output.push(ob.rv);
+    }
+    for ob in &observations {
+        output.extend(&ob.bisector);
+    }
+    let ptr = output.as_ptr();
+    std::mem::forget(output);
+    ptr
+}
