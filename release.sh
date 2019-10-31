af118bd1adb82f3996bf6bb4447524537cc2be60
Ben Kimock <kimockb@gmail.com>
2019-10-31 13:30:57 -0400

    The wheel is lather- not rust-lather-

--- release.sh
+++ release.sh
@@ -11,5 +11,5 @@ cd wheelhouse
 COMMIT_HASH=$(git log -n 1 --pretty=format:"%H" | cut -c-7)
 curl -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" --data "{\"tag_name\": \"${COMMIT_HASH}\"}" https://api.github.com/repos/saethlin/rust-lather/releases >  /tmp/response
 ID=$(python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj["id"])' < /tmp/response)
-WHEEL=$(ls rust-lather*.whl)
+WHEEL=$(ls lather*.whl)
 curl -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" -H "Content-type: application/octet-stream" --data @${WHEEL} "https://uploads.github.com/repos/saethlin/rust-lather/releases/${ID}/assets?name=${WHEEL}"
